89b1003fc87ae08ee6806b6500231a56688903c0
Jean Vancoppenolle <vancoppenolle@ferret-go.com>
2018-06-25 12:51:41 +0200

    Refactor health checks; introduce check timeout.

--- limonado/contrib/health.py
+++ limonado/contrib/health.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 import functools
 import time
-import weakref
 
 from tornado.gen import coroutine
+from tornado.gen import with_timeout
+import tornado.util
 
 from ..core.api import API
 from ..core.api import APIHandler
+
+_TIMEOUT = datetime.timedelta(seconds=30)
 
 _HEALTH_PARAMS = {
     "additionalProperties": False,
     "type": "object",
     "properties": {
-        "check": {
-            "type": "array",
-            "itemSeparator": ",",
-            "items": {
-                "type": "string"
-            }
+        "failed_status": {
+            "type": "integer",
+            "minimum": 500,
+            "maximum": 599
         }
     }
 }
@@ -59,103 +61,88 @@ def cache_health(ttl):
 
 class HealthHandler(APIHandler):
     @coroutine
-    def head(self):
-        health = yield self.check_health()
-        if health["status"] == "unhealthy":
-            self.set_status(self.api.unhealthy_status)
-
-        self.finish()
-
-    @coroutine
     def get(self):
-        health = yield self.check_health()
-        if health["status"] == "unhealthy":
-            self.set_status(self.api.unhealthy_status)
+        params = self.get_params(_HEALTH_PARAMS)
+        health = yield self.api.check_health()
+        if health["status"] == "failed":
+            self.set_status(params.get("failed_status", 200))
 
         self.write_json(health)
         self.finish()
 
+
+class BaseHealthAPI(API):
+    @property
+    def handlers(self):
+        return [("", HealthHandler)]
+
+    @property
+    def timeout(self):
+        return _TIMEOUT
+
+    @property
+    def checks(self):
+        return {}
+
     @coroutine
     def check_health(self):
-        params = self.get_params(_HEALTH_PARAMS)
-        issues = yield self.api.check_health(include=params.get("check"))
-        status = "unhealthy" if issues else "healthy"
+        issues = yield self._get_issues()
+        if issues:
+            status = "failed"
+        else:
+            status = "ok"
+
         return {"status": status, "issues": issues}
 
+    @coroutine
+    def _get_issues(self):
+        # Run checks in parallel.
+        results = yield {
+            name: self._run_check(check)
+            for name, check in self.checks.items()
+        }
+        return {
+            name: result
+            for name, result in results.items() if result is not None
+        }
 
-class HealthAPI(API):
-    def __init__(self,
-                 parent=None,
-                 prefix="/health",
-                 handler_class=HealthHandler,
-                 unhealthy_status=503,
-                 checks=None,
-                 **kwargs):
+    @coroutine
+    def _run_check(self, check):
+        future = check()
+        if self.timeout is not None:
+            future = with_timeout(
+                self.timeout, future, quiet_exceptions=(HealthIssue, ))
+
+        try:
+            yield future
+        except tornado.util.TimeoutError:
+            return {"reason": "Check timed out."}
+        except HealthIssue as exc:
+            return {"reason": str(exc)}
+
+
+class HealthAPI(BaseHealthAPI):
+    def __init__(self, *, timeout=_TIMEOUT, checks=None, **kwargs):
         super().__init__(**kwargs)
-        if parent is None:
-            self._parent = None
+        if hasattr(timeout, "total_seconds"):
+            self._timeout = timeout
         else:
-            self._parent = weakref.proxy(parent)
+            self._timeout = datetime.timedelta(seconds=timeout)
 
-        self._prefix = prefix
-        self._handler_class = handler_class
-        self._unhealthy_status = unhealthy_status
-        if checks is None:
-            self._checks = {}
-        elif callable(checks):
-            self._checks = checks(self)
-        else:
-            self._checks = dict(checks)
+        self._checks = {} if checks is None else checks.copy()
 
     @property
-    def parent(self):
-        return self._parent
-
-    @property
-    def prefix(self):
-        return self._prefix
-
-    @property
-    def handler_class(self):
-        return self._handler_class
-
-    @property
-    def unhealthy_status(self):
-        return self._unhealthy_status
+    def timeout(self):
+        return self._timeout
 
     @property
     def checks(self):
         return self._checks
 
-    @property
-    def handlers(self):
-        return [("", self._handler_class)]
-
-    @coroutine
-    def check_health(self, include=None):
-        issues = {}
-        for name, check in self.checks.items():
-            if include is None or name in include:
-                try:
-                    yield check(self)
-                except HealthIssue as issue:
-                    issues[name] = {
-                        "message": issue.message,
-                        "details": issue.details
-                    }
-
-        return issues
+    def add_check(self, name, func, *args, **kwargs):
+        self._checks[name] = functools.partial(func, *args, **kwargs)
+        return self
 
 
 class HealthIssue(Exception):
-    def __init__(self, message, details=None):
-        self._message = message
-        self._details = {} if details is None else dict(details)
-
-    @property
-    def message(self):
-        return self._message
-
-    @property
-    def details(self):
-        return self._details
+    pass
